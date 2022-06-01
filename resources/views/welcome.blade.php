0244fe909d46084329dd6f9a3a746d523f25db3d
nicolasandrada <nicolas.andrada@hotmail.com>
2022-05-31 21:29:57 -0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EEST 11</title>
+</head>
+<body>
+    <h1>EEST 11 - Islas Malvinas </h1>
+    <div>
+        <h2>Docentes en lista </h2>
+        @foreach ($docentes as $d)
+        <div>
+            <h3>id {{$d->id}}</h3>
+            <h3>nombre {{$d->nombre}}</h3>
+            <h3>apellido {{$d->apellido}}</h3>
+        </div>
+        @endforeach
+
+    </div>
+</body>
+</html>
